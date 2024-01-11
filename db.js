80ebc19034f0a59c7ae38befa6e60eab584a2525
Pranshu Tomer <136837700+pranshu-tomer@users.noreply.github.com>
2024-01-11 11:08:04 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-mongoose.connect("mongodb://127.0.0.1:27017/nit-students").then(() => {
+mongoose.connect(process.env.Mongo_Url).then(() => {
     console.log("Connected to Server");
 })
 .catch((err) => {
     console.log('Error occured ' + err);
 })
-
-// process.env.Mongo_Url
